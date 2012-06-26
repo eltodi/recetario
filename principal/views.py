ff803185330dc3492d7a6e4bd9a8855da8c04ed2
Elias Torres <eltodi@gmail.com>
2012-06-26 14:57:25 +0200

    Capitulo VISTAS

--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,27 @@
-from principal.models import Bebida
-from django.shortcuts import render_to_response
+from principal.models import Receta, Comentario
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.contrib.auth.models import User
 
-def lista_bebidas(request):
-	bebidas = Bebida.objects.all()
-	return render_to_response("lista_bebidas.html",{"lista":bebidas})
+def sobre(request):
+	html = "<html><body>Hola Mundo</body<</html>";
+	return HttpResponse(html)
+
+def inicio(request):
+	recetas = Receta.objects.all()
+	return render_to_response("inicio.html",{"datos" : recetas})
+
+def usuarios(request):
+	usuarios = User.objects.all()
+	recetas = Receta.objects.all()
+	return render_to_response("usuarios.html",{"usuarios" : usuarios, "recetas" : recetas })
+
+def recetas(request):
+	recetas = Receta.objects.all()
+	return render_to_response("recetas.html", {"recetas" : recetas}, context_instance=RequestContext(request))
+
+def detalle_receta(request, id_receta):
+	receta = get_object_or_404(Receta, pk=id_receta)
+	comentarios = Comentario.objects.filter(receta=receta)
+	return render_to_response("receta.html", {"receta" : receta, "comentarios" : comentarios}, context_instance=RequestContext(request))	
--- urls.py
+++ urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$','principal.views.lista_bebidas'),
+	url(r'^$','principal.views.inicio'),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT,}),
+
+	url(r'^sobre/$','principal.views.sobre'),
+	url(r'^usuarios/$', 'principal.views.usuarios'),
+	url(r'^recetas/$', 'principal.views.recetas'),
+	url(r'^receta/(?P<id_receta>\d+)$','principal.views.detalle_receta'),
 )
